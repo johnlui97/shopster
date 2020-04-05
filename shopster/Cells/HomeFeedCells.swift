6489284508a33f28a3cdf3ac395b087fc5af4e9a
johnlui97 <johnlui97@gmail.com>
2020-04-05 19:17:23 -0400

    Added HomeFeedCells which includes profile image, product title, product owner, date posted. As well as a collection view skeleton that will hold product images that can scroll horizontally.

---
+++ shopster/Cells/HomeFeedCells.swift
@@ -0,0 +1,146 @@
+//
+//  HomeFeedCells.swift
+//  shopster
+//
+//  Created by John Lui on 2020-04-05.
+//  Copyright © 2020 John Lui. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedCells: UICollectionViewCell {
+    
+    //UI Elements
+    
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemRed
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var topContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var labelVerticalStack: UIStackView = {
+        let stack = UIStackView()
+        stack.addArrangedSubview(productTitle)
+        stack.addArrangedSubview(productOwner)
+        stack.addArrangedSubview(productPostTime)
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    lazy var ownerImage: UIImageView = {
+        let image = UIImageView()
+        let picture = UIImage(named: "defaultAvatar")
+        image.image = picture
+        image.contentMode = .scaleAspectFit
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = (self.frame.height/5)/2
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var productTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Sameple Product"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    lazy var productOwner: UILabel = {
+        let label = UILabel()
+        label.text = "Sameple Owner"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    lazy var productPostTime: UILabel = {
+        let label = UILabel()
+        label.text = "Sameple Time Stamp"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    lazy var productCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell_id")
+        view.backgroundColor = .systemGreen
+        return view
+    }()
+    
+    //Constraints Methods
+    
+    private func constraintContainerView(childView: UIView, parentView: UIView) {
+        childView.topAnchor.constraint(equalTo: parentView.topAnchor, constant: 12).isActive = true
+        childView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor, constant: 12).isActive = true
+        childView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor, constant: -12).isActive = true
+        childView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor, constant: -12).isActive = true
+    }
+    
+    private func constraintTopContainerView(childView: UIView, parentView: UIView) {
+        childView.topAnchor.constraint(equalTo: parentView.topAnchor).isActive = true
+        childView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
+        childView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
+        childView.heightAnchor.constraint(equalToConstant: self.frame.height/5).isActive = true
+    }
+    
+    private func constraintOwnerImage(childView: UIImageView, parentView: UIView) {
+        childView.centerYAnchor.constraint(equalTo: parentView.centerYAnchor).isActive = true
+        childView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
+        childView.heightAnchor.constraint(equalToConstant: self.frame.height/5).isActive = true
+        childView.widthAnchor.constraint(equalToConstant: self.frame.height/5).isActive = true
+    }
+    
+    private func constraintLabelStack(childView: UIStackView, parentView: UIView, referenceView: UIImageView) {
+        childView.topAnchor.constraint(equalTo: parentView.topAnchor).isActive = true
+        childView.leadingAnchor.constraint(equalTo: referenceView.trailingAnchor, constant: 6).isActive = true
+        childView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
+    }
+    
+    private func constraintProductCollectionView(childView: UICollectionView, parentView: UIView, referenceView: UIView) {
+        childView.topAnchor.constraint(equalTo: referenceView.bottomAnchor).isActive = true
+        childView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
+        childView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
+        childView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+            
+        addSubview(containerView)
+        constraintContainerView(childView: containerView, parentView: self)
+        
+        containerView.addSubview(topContainerView)
+        constraintTopContainerView(childView: topContainerView, parentView: containerView)
+        
+        topContainerView.addSubview(ownerImage)
+        constraintOwnerImage(childView: ownerImage, parentView: topContainerView)
+        
+        topContainerView.addSubview(labelVerticalStack)
+        constraintLabelStack(childView: labelVerticalStack, parentView: topContainerView, referenceView: ownerImage)
+        
+        containerView.addSubview(productCollectionView)
+        constraintProductCollectionView(childView: productCollectionView, parentView: containerView, referenceView: topContainerView)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
--- shopster/ViewControllers/HomeFeedViewController.swift
+++ shopster/ViewControllers/HomeFeedViewController.swift
@@ -5,12 +5,43 @@
 //  Created by John Lui on 2020-04-05.
 //  Copyright © 2020 John Lui. All rights reserved.
 //
-
 import UIKit
 
 class HomeFeedViewController: UIViewController {
+
+    lazy var homeFeedCollectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let view = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(HomeFeedCells.self, forCellWithReuseIdentifier: "cell_id")
+        view.backgroundColor = .systemOrange
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemOrange
+        view.addSubview(homeFeedCollectionView)
+        homeFeedCollectionView.delegate = self
+        homeFeedCollectionView.dataSource = self
     }
+    
+}
+
+extension UIViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height/3)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_id", for: indexPath) as! HomeFeedCells
+        cell.backgroundColor = .systemYellow
+        return cell
+    }
+    
 }
