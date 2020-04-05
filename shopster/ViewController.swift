9c19b6e6b50b9975236567869a961794525e70f5
johnlui97 <johnlui97@gmail.com>
2020-04-05 14:05:15 -0400

    Initial Commit

---
+++ shopster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopster
+//
+//  Created by John Lui on 2020-04-05.
+//  Copyright © 2020 John Lui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
