a6eef4bae1273081290f6567e4cabbe0e0dde162
johnlui97 <johnlui97@gmail.com>
2020-04-08 22:02:44 -0400

    Added collectionview skeleton onto product view, as well laid out design requirements in Figma.

--- shopster/ViewControllers/ProductViewController.swift
+++ shopster/ViewControllers/ProductViewController.swift
@@ -10,10 +10,52 @@ import UIKit
 
 class ProductViewController: UIViewController {
     
+    var cellID = "product_view_cell_id"
     
+    lazy var productImageCollectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        view.isPagingEnabled = true
+        view.showsHorizontalScrollIndicator = false
+        view.backgroundColor = .systemOrange
+        return view
+    }()
+    
+    private func constraintProductImageCollectionView() {
+        productImageCollectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        productImageCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        productImageCollectionView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        productImageCollectionView.heightAnchor.constraint(equalToConstant: view.frame.height/2).isActive = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        view.addSubview(productImageCollectionView)
+        constraintProductImageCollectionView()
+        productImageCollectionView.delegate = self
+        productImageCollectionView.dataSource = self
     }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = indexPath.row%2==0 ? .systemGreen : .systemRed
+        return cell
+    }
+    
+    internal func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
 }
