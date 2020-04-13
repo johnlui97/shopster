6d4ec5d02b8ae87fea41c0de1a10a7510aa42e0f
johnlui97 <johnlui97@gmail.com>
2020-04-12 21:52:10 -0400

    Added a collectionview skeleton for the explore section. Need to figure out how to dynamic size cell based on its content. Will continue Tomorrow.

--- shopster/ViewControllers/ExploreViewController.swift
+++ shopster/ViewControllers/ExploreViewController.swift
@@ -9,8 +9,51 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
+    
+    var cellID = "explore_cellID"
+    
+    lazy var exploreCollectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        view.showsVerticalScrollIndicator = false
+        view.backgroundColor = .systemOrange
+        return view
+    }()
+    
+    private func constraintExploreCollectionView() {
+        exploreCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        exploreCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        exploreCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        exploreCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
+        view.backgroundColor = .white
+        view.addSubview(exploreCollectionView)
+        constraintExploreCollectionView()
+        exploreCollectionView.delegate = self
+        exploreCollectionView.dataSource = self
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        return
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height/4)
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    internal override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .systemIndigo
+        return cell
     }
 }
